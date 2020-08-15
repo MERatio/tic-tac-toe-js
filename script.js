180d29e7830da623f7ef9d9f02699729eb2d2520
Mark Edward Ratio <markedwardratio@gmail.com>
2020-08-15 13:34:58 +0800

    Allow players to mark specific square

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ const dom = (() => {
 })();
 
 const board = (() => {
-  let squaresValues = ['X', '0', 'X', '0', 'X', '0', 'X', '0', 'X'];
+  let squaresValues = Array(9).fill('');
+
+  const _isSquareEmpty = (index) => squaresValues[index] === '';
 
   const getsquaresValues = () => squaresValues;
 
+  let updateSquare = (index, player) => {
+    if (_isSquareEmpty(index)) {
+      squaresValues[index] = player;
+      display.populateSquare();
+      return true;
+    }
+  };
+
   return {
     getsquaresValues,
+    updateSquare,
   };
 })();
 
@@ -28,4 +39,35 @@ const display = (() => {
   };
 })();
 
-display.populateSquare();
+const game = (() => {
+  let currentPlayer = 'X';
+
+  const _changeCurrentPlayer = () => {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+  };
+
+  const _domSquareClick = (e) => {
+    let domSquaresArr = [...dom.squares];
+    let squareIndex = domSquaresArr.indexOf(e.target);
+    if (board.updateSquare(squareIndex, currentPlayer)) {
+      _changeCurrentPlayer();
+    }
+  };
+
+  const _attachEvents = () => {
+    dom.squares.forEach((domSquare) => {
+      domSquare.addEventListener('click', _domSquareClick);
+    });
+  };
+
+  const init = () => {
+    display.populateSquare();
+    _attachEvents();
+  };
+
+  return {
+    init,
+  };
+})();
+
+game.init();
